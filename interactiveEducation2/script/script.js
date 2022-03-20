2ba40e35279e580ffb711e97810e08cd0144f8c4
Liane Nguyen <liane.ng.ng@gmx.com>
2022-03-20 18:33:01 +0100

    text hinzugefügt + Gif hinzugefügt

--- interactiveEducation2/script/script.js
+++ interactiveEducation2/script/script.js
@@ -1,16 +1,26 @@
 import {Gemaelde} from "./model/gemaelde.js";
 
 let gemaelde = [];
-gemaelde.push(new Gemaelde(1, './images/gemaelde1.jpg', 'Michelangelo', 'Vincent Van Gogh', 'Leonardo da Vinci', 'Wer ist der Künstler?', 'antwort1', 'Die Erschaffung'));
-gemaelde.push(new Gemaelde(2, './images/gemaelde2.jpg', 'Claude Monet', 'Leonardo da Vinci', 'Albrecht Dürer', 'Wer ist der Künstler?', 'antwort2', 'Mona Lisa'));
-gemaelde.push(new Gemaelde(3, './images/gemaelde3.jpg', 'Pablo Picasso', 'Vincent Van Gogh', 'Johannes Vermeer', 'Wer ist der Künstler?', 'antwort3', 'Das Mädchen mit dem Perlenohrring'));
-gemaelde.push(new Gemaelde(4, './images/gemaelde4.jpg', 'Peter Paul Rubens', 'Georges Seurat', 'Pablo Picasso', 'Wer ist der Künstler?', 'antwort3', 'Guernica'));
-gemaelde.push(new Gemaelde(5, './images/gemaelde5.jpg', 'Claude Monet', 'Salvador Dali', 'Vincent Van Gogh', 'Wer ist der Künstler?', 'antwort1'));
-gemaelde.push(new Gemaelde(6, './images/gemaelde6.jpg', 'Edward Munch', 'Peter Paul Rubens', 'Salvador Dali', 'Wer ist der Künstler?', 'antwort3','Die Beständigkeit der Erinnerung'));
-gemaelde.push(new Gemaelde(7, './images/gemaelde7.jpg', 'Georges Seurat', 'Michelangelo', 'Salvador Dali', 'Wer ist der Künstler?', 'antwort1', 'Ein Sonntagnachmittag auf der Insel La Grande Jatte'));
-gemaelde.push(new Gemaelde(8, './images/gemaelde8.jpg', 'Albrecht Dürer', 'Vincent Van Gogh', 'Edward Munch', 'Wer ist der Künstler?','antwort3','Der Schrei'));
-gemaelde.push(new Gemaelde(9, './images/gemaelde9.jpg', 'Michelangelo', 'Leonardo da Vinci', 'Pablo Picasso', 'Wer ist der Künstler?', 'antwort1', 'Das letzte Abendmahl'));
-gemaelde.push(new Gemaelde(10, './images/gemaelde10.jpg', 'Pablo Picasso', 'Vincent Van Gogh', 'Claude Monet', 'Wer ist der Künstler?', 'antwort2', 'Sternennacht'));
+gemaelde.push(new Gemaelde(1, './images/gemaelde1.jpg', 'Michelangelo', 'Vincent Van Gogh', 'Leonardo da Vinci', 'Wer ist der Künstler?', 'antwort1', 'Die Erschaffung', 
+    'Es ist während der italienischen Hochrenaissance entstanden und ist eines der berühmtesten Gemälde aus dieser Epoche und eines der berühmtesten aller Zeiten. Du kannst sie über die Vatikanischen Museen betreten. Es wurde 1508 und 1512 in der Sixtinischen Kapelle in Rom erschaffen.'));
+gemaelde.push(new Gemaelde(2, './images/gemaelde2.jpg', 'Claude Monet', 'Leonardo da Vinci', 'Albrecht Dürer', 'Wer ist der Künstler?', 'antwort2', 'Mona Lisa', 
+    'Das Ölgemälde ist aus dem 16. Jahrhundert und eines der berühmtesten der Welt. Seit 1815 hängt das Kunstwerk im königlichen Palais du Louvre in Paris. Das besondere und größte Gespärchstthema ist Mona Lisa´s geheimnisvolles Lächeln.'));
+gemaelde.push(new Gemaelde(3, './images/gemaelde3.jpg', 'Pablo Picasso', 'Vincent Van Gogh', 'Johannes Vermeer', 'Wer ist der Künstler?', 'antwort3', 'Das Mädchen mit dem Perlenohrring', 
+    'Es ist sein berühmtestes Gemälde aus dem Jahr 1665. Das Mädchen auf dem Bild war eine Magd, die für den Künstler gearbeitet hat. Es wird aber auch vermutet, dass es die älteste Tochter des Künstlers ist.'));
+gemaelde.push(new Gemaelde(4, './images/gemaelde4.jpg', 'Peter Paul Rubens', 'Georges Seurat', 'Pablo Picasso', 'Wer ist der Künstler?', 'antwort3', 'Guernica', 
+    'Seine Vorlage für das Bild wurde das Bombardement der Nationalsozialisten der gleichnamigen Stadt in Nordspanien. Durch sein Werk wollte er die Taten der Faschisten und die Gewalt des Krieges aufzeigen. Der Maler kreierte es für die Weltausstellung in Paris 1937 innerhalb von zwei Wochen.'));
+gemaelde.push(new Gemaelde(5, './images/gemaelde5.jpg', 'Claude Monet', 'Salvador Dali', 'Vincent Van Gogh', 'Wer ist der Künstler?', 'antwort1', 'Impression, Sonnenaufgang', 
+    'Das Bild zeigt den Hafen von Le Havre am Morgen. Das Bild ist so ebenmäßig gemalt, dass der Anschein der räumlichen Distanz ausschließlich durch die schräg angeordneten Boote deutlich wird. Der Künstler verzichtet auf Komposition und räumliche Wirkung. Ziel der Darstellung ist die unverfälschte Wiedergabe des momentanen objektiven Seh-Eindruckes.'));
+gemaelde.push(new Gemaelde(6, './images/gemaelde6.jpg', 'Edward Munch', 'Peter Paul Rubens', 'Salvador Dali', 'Wer ist der Künstler?', 'antwort3','Die Beständigkeit der Erinnerung',
+    'Es ist eine Interpretation, wie sich Träume anfühlen. Er beschrieb diese Art der Darstellung als handgemalte Traumfotographien. Bist du auch etwas verwirrt beim Anblick? Genau diese Reaktion wollte der Künstler beim Betrachter erreichen und ihn zum Überdenken der kompletten Welt bringen.'));
+gemaelde.push(new Gemaelde(7, './images/gemaelde7.jpg', 'Georges Seurat', 'Michelangelo', 'Salvador Dali', 'Wer ist der Künstler?', 'antwort1', 'Ein Sonntagnachmittag auf der Insel La Grande Jatte', 
+    'Es hat den Künstler zwei Jahre gebraucht, bis er Farbtupfer für Farbtupfer sein berühmtestes und zwei mal drei Meter großes Werk erschuf. Farbtupfer? Du hast richtig gehört! Dieses Bild ist im Stil des Pointillismus gemalt. Es besteht also aus einzelnen, kleinen Farbtupfern reiner Farben. Die ungemischten Farben erzeugen eine stärkere Leuchtkraft als gemischte.'));
+gemaelde.push(new Gemaelde(8, './images/gemaelde8.jpg', 'Albrecht Dürer', 'Vincent Van Gogh', 'Edward Munch', 'Wer ist der Künstler?','antwort3','Der Schrei',
+    'Es gehört zu einer Serie von expressionistischer Gemälde. Sie zeigen den Oslofjord - ein Fjord in Südnorwegen - und eine schreiende Person. Im Sommer 1891 soll ein blutroter Sonnenuntergang ihn bei einem Spaziergang schrecklich erschreckt haben.'));
+gemaelde.push(new Gemaelde(9, './images/gemaelde9.jpg', 'Michelangelo', 'Leonardo da Vinci', 'Pablo Picasso', 'Wer ist der Künstler?', 'antwort1', 'Das letzte Abendmahl',
+    'Das legendäre Fresko kann man nun wieder in der Renaissancekirche Santa Maria delle Grazie in Mailand bewundern. Im ehemaligen Speisesaal des anliegenden Klosters kreierte der Künstler von 1494 bis 1498 das Wandgemälde. Es wurde über die letzten 20 Jahre restauriert und ist jetzt wieder für die Öffentlichkeit zugänglich.'));
+gemaelde.push(new Gemaelde(10, './images/gemaelde10.jpg', 'Pablo Picasso', 'Vincent Van Gogh', 'Claude Monet', 'Wer ist der Künstler?', 'antwort2', 'Sternennacht',
+    'Es ist eines seiner berühmtesten Werke. Mittlerweile hängt das Ölgemälde von 1889 im Museum of Modern Art in New York City. Wusstest du, dass die Wirbel des Sternenhimmels im Bild eines der komplexesten mathematischen Phänomene darstellen? Die Turbulenz.'));
 /* Richtige Antworten
 1: Michelangelo
 2: Leonardi da Vinci
@@ -33,7 +43,7 @@ let vm = new Vue({
         currentPic: gemaelde[0],
         punktestand: 0,
         gemaelde_done: [],
-        bild_counter:0,
+        bild_counter: 0,
     },
     methods:{
         getRandomInt(min,max){
@@ -47,18 +57,20 @@ let vm = new Vue({
             let randomCounter = this.getRandomInt(1,10);
             console.log(randomCounter);
 
-            if(this.gemaelde_done.includes(randomCounter)){
-                this.nextpic();
-            } else {
-                this.currentPic =  this.gemaelde.find(element => element.counter === randomCounter);
-            }
+            if(this.bild_counter < 10){
+                if(this.gemaelde_done.includes(randomCounter)){
+                    this.nextpic();
+                } else {
+                    this.currentPic =  this.gemaelde.find(element => element.counter === randomCounter);
+                    this.bild_counter += 1;
+                }
+            }   
             
             
             console.log(this.currentPic);
 
             this.nextBild = true;
             this.gemaelde_done.push(this.currentPic.counter);
-            this.bild_counter += 1;
             
             //change color back to black
             document.getElementById("antwort1").style.backgroundColor='#000000';
